df2e04b302bb28904910dc83ac2a32f58250485a
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-20 02:26:28 +0100

    update

--- run_load_balancing_test.bat
+++ run_load_balancing_test.bat
@@ -1,6 +1,6 @@
 start cmd /k run.bat
 TIMEOUT /t 3
-start cmd /k php -S localhost:10001 -t test\1
-start cmd /k php -S localhost:10002 -t test\2
+start cmd /k python -m http.server 10001 --directory test/1
+start cmd /k python -m http.server 10002 --directory test/2
 start cmd /k python -m worker.main --target-hostname http://localhost:10001/
 start cmd /k python -m worker.main --target-hostname http://localhost:10002/
