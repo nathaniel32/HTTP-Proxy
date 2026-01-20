284b68f85e08f50c1900b3c34873b971cf6ce411
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-20 02:23:48 +0100

    update

--- run.bat
+++ run.bat
@@ -1,5 +1,1 @@
 start cmd /k uvicorn server.main:app --host 0.0.0.0 --port 8080
-TIMEOUT /t 3
-start cmd /k python -m worker.main
-TIMEOUT /t 3
-start cmd /k node test/test_ollama.js
---
+++ run_ollama.bat
@@ -0,0 +1,3 @@
+start cmd /k run.bat
+TIMEOUT /t 3
+start cmd /k python -m worker.main
